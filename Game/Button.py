e58eec07df91ec6e9175605b1af2dece57d19630
jharia <jesikaharia@gmail.com>
2012-12-13 19:47:24 -0500

    First commit, TOODO: code cleanup

---
+++ Game/Button.py
@@ -0,0 +1,65 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+#
+# CLICKABLE BUTTON WITH AN IMAGE INSIDE IT
+#
+class Button:
+	def __init__(self,x,y,imagefile):
+		self.visible = True
+		# load in the icon that will go in the middle of the button
+		self.insideimg = pygame.image.load(imagefile).convert_alpha()
+		self.insiderect = self.insideimg.get_rect()
+		self.ix = self.insiderect.size[0]
+		self.iy = self.insiderect.size[1]
+
+		# set up base values for the size of this button to the image inside it
+		self.image = pygame.surface.Surface((self.ix+8,self.iy+8))
+		self.rect = self.image.get_rect()
+		self.rect.left = x
+		self.rect.top = y
+
+		# set button as unpressed
+		self.popup()
+
+	# draw the button in an "unpressed" state
+	def popup(self):
+		if self.visible: 
+			self.image.fill((96,96,96))
+			pygame.draw.line(self.image, (224,224,224), (0,0),(self.ix+8,0) )
+			pygame.draw.line(self.image, (224,224,224), (0,0),(0,self.ix+8) )
+			pygame.draw.line(self.image, (224,224,224), (1,1),(self.ix+7,1) )
+			pygame.draw.line(self.image, (224,224,224), (1,1),(1,self.ix+7) )
+			pygame.draw.line(self.image, (224,224,224), (2,2),(self.ix+6,2) )
+			pygame.draw.line(self.image, (224,224,224), (2,2),(2,self.ix+6) )
+			pygame.draw.rect(self.image, (160,160,160), ((3,3),(self.ix+3,self.iy+3)) )
+			
+			# put the button icon on the button
+			self.image.blit(self.insideimg,(4,4))
+
+	# draw the button in a "pressed" state
+	def press(self):
+		if self.visible: 
+			self.image.fill((224,224,224))
+			pygame.draw.line(self.image, (96,96,96), (0,0),(self.ix+8,0) )
+			pygame.draw.line(self.image, (96,96,96), (0,0),(0,self.ix+8) )
+			pygame.draw.line(self.image, (96,96,96), (1,1),(self.ix+7,1) )
+			pygame.draw.line(self.image, (96,96,96), (1,1),(1,self.ix+7) )
+			pygame.draw.line(self.image, (96,96,96), (2,2),(self.ix+6,2) )
+			pygame.draw.line(self.image, (96,96,96), (2,2),(2,self.ix+6) )
+			pygame.draw.rect(self.image, (160,160,160), ((3,3),(self.ix+3,self.iy+3)) )
+		
+			# put the button icon on the button
+			self.image.blit(self.insideimg,(4,4))
+
+	# this gets called if the mouse is clicked - check if the button was hit
+	def clicked(self,pos):
+		if self.visible: 
+			if (self.rect.left < pos[0] < self.rect.right) and (self.rect.top < pos[1] < 
+				self.rect.bottom):
+				return 1
+
+	# method for disposing button 
+	def dispose(self): 
+		self.visible = False
---
+++ Game/newGame.py
@@ -0,0 +1,227 @@
+#! /usr/bin/env python
+'''
+TOOF: Implement 
+1. log of all actions at the end of a game before sys quit 
+2. 
+'''
+import os, sys
+import pygame
+from pygame.locals import *
+from Button import *
+import math, random
+import eztext
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+class TestGame:
+    """The Main PyMan Class - This class handles the main 
+    initialization and creating of the Game."""
+    
+    def __init__(self, width=640,height=480):
+        """Initialize"""
+        """Initialize PyGame"""
+        pygame.init()
+        """Set the window Size"""
+        self.width = width
+        self.height = height
+        self.mult = 0 
+        self.score = 0 
+        """Create the Screen"""
+        self.screen = pygame.display.set_mode((self.width
+                                               , self.height))
+        pygame.display.set_caption('The Length Estimation Challenge')
+
+        # set up the colors 
+        self.BLACK = (  0,   0,   0)
+        self.WHITE = (255, 255, 255)
+        self.RED = (255,   0,   0)
+        self.GREEN = (  0, 255,   0)
+        self.BLUE = (  0,   0, 255)
+        self.GRAY = (  200,200,200)
+                                                          
+    def MainLoop(self):
+        """Create the background"""
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.screen.fill((255,255,255))
+
+        # timer stuff 
+        clock = pygame.time.Clock()
+        frame_count = 0
+        frame_rate = 20
+        start_time = 60
+
+        while True: # main game loop
+
+            font = pygame.font.Font(None, 30)
+            # --- Timer going down ---
+            # --- Timer going up ---
+            # Calculate total seconds
+            total_seconds = start_time - (frame_count // frame_rate)
+            if total_seconds < 0:
+                total_seconds = 0
+             
+            # Divide by 60 to get total minutes
+            minutes = total_seconds // 60
+             
+            # Use modulus (remainder) to get seconds
+            seconds = total_seconds % 60
+             
+            # Use python string formatting to format in leading zeros
+            output_string = "Time left: {0:02}:{1:02}".format(minutes,seconds)
+            
+            # when timer runs out 
+            if total_seconds == 0: 
+                pygame.draw.rect(self.screen, self.GRAY, pygame.Rect(0, 0, 640, 480))
+                text = font.render("Game over, thanks for playing! Your score is: "+str(self.score),1,self.RED)
+                x ,y = self.screen.get_size()
+                self.screen.blit(text, [50,y/2])
+                pygame.display.flip() 
+                pygame.time.wait(1000)
+                sys.exit()
+
+            # warning towards the end of the game
+            rectCol = self.WHITE 
+            # Blit to the screen
+            if total_seconds <= 5 and total_seconds > 0: 
+                if frame_count%20 == 0: 
+                    rectCol = self.RED 
+                else : 
+                    rectCol = self.WHITE
+            pygame.draw.rect(self.screen, rectCol, pygame.Rect(425, 0, 240, 40))
+            text = font.render(output_string,1,self.BLACK)
+            self.screen.blit(text, [450,10])
+            pygame.display.flip() 
+
+            '''timer = 0 
+            seconds = clock.tick(40)/1000.0
+            timer += seconds 
+            displaytimer = math.trunc(timer)
+            myfont = pygame.font.SysFont(None, 30)
+            # apply it to text on a label
+            label_time = myfont.render("Timer: "+str(displaytimer), 1, GREEN)
+            # put the label object on the screen at point x=100, y=100
+            self.screen.blit(label_time, (500, 450))
+            pygame.display.flip()'''
+
+            gobut = Button(10,10,"goMed.png")
+            txtbx = eztext.Input(x=70,y=10,maxlength=2, color=(0,255,0), prompt='Type your guess: ')
+            self.screen.blit(gobut.image,gobut.rect)
+            pygame.display.flip() #update display
+
+
+
+            k = pygame.event.poll() #look for an event
+            if k.type == MOUSEBUTTONDOWN: #if they click,
+                if k.button == 1: #if it's the left button,  
+                    if gobut.clicked(k.pos): #check if the button's limits are clicked
+                        self.screen.fill(self.WHITE)
+                        pygame.display.flip()
+
+                        gobut.press() #depress the button
+                        self.screen.blit(gobut.image,gobut.rect) #update the screen
+                        print "they clicked the go button"
+                        pygame.display.flip() #update display
+
+                        self.generateRandom()
+ 
+                        # update txtbx
+                        txtbx.update([k])
+                        # blit txtbx on the sceen
+                        txtbx.draw(self.screen)
+                        # refresh the display
+                        pygame.display.flip()
+
+                        
+
+            elif k.type == KEYDOWN: 
+                if k.key == 13: 
+                    print "entered enter"
+                    # TODO: send the current guess to the log 
+                    # refresh the page ie. get new problem 
+                    # generate random measurement and reference lines and positions 
+                    self.screen.fill(self.WHITE)
+                    pygame.display.flip()
+                    self.generateRandom()
+                    pygame.display.flip()
+                    generatedMouse1 = pygame.event.Event(MOUSEBUTTONDOWN,{'pos':(11,11),'button':gobut})
+                    generatedMouse2 = pygame.event.Event(MOUSEBUTTONUP,{'pos':(11,11),'button':gobut})
+                    pygame.event.post(generatedMouse1)
+                    pygame.event.post(generatedMouse2)
+
+                    # display the score 
+                    myfont = pygame.font.SysFont(None, 30)
+                    # apply it to text on a label
+                    label = myfont.render("Score: "+str(self.score), 1, self.BLUE)
+                    # put the label object on the screen at point x=100, y=100
+                    self.screen.blit(label, (300, 10))
+                    pygame.display.flip()
+
+                # update txtbx
+                txtbx.update([k])
+                # blit txtbx on the sceen
+                txtbx.draw(self.screen)
+                # refresh the display
+                pygame.display.flip()
+                #peek at value 
+                print "USER ENTERED: ", txtbx.value
+                print "MULT RIGHT HERE", self.mult
+                
+                if k.key != 13 and int(txtbx.value) == self.mult: 
+                    print 'Trueee'
+                    self.score += self.mult 
+
+            frame_count += 1
+            # Limit to 20 frames per second
+            clock.tick(frame_rate)
+          
+            # Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
+
+
+            if k.type == QUIT:
+                    pygame.quit()
+                    sys.exit()   
+            pygame.display.update()
+
+            '''# drawing the first bar! :) 
+            self.drawBar(GRAY,60,120,60)
+            self.drawBar(RED,120,250,240)'''
+
+    def drawBar(self,color,left,right,y): 
+        pygame.draw.line(self.screen, color, (left, y), (right, y), 4)
+        pygame.draw.line(self.screen, color, (left, y-7), (left, y+7), 4)
+        pygame.draw.line(self.screen, color, (right, y-7), (right, y+7), 4)
+
+
+    def generateRandom(self): 
+        # generate random measurement and reference lines and positions 
+        self.left_x1 = random.randint(50,400)
+        self.right_x1 = random.randint(self.left_x1+10,self.left_x1+20)
+        self.y_x1 = random.randint(50,400)
+        self.drawBar(self.GRAY,self.left_x1,self.right_x1,self.y_x1)
+        # label the reference line 
+        refLineFont = pygame.font.SysFont(None, 25)
+        refLabel = refLineFont.render("1 unit", 1, self.GRAY)
+        self.screen.blit(refLabel, (self.left_x1, self.y_x1 + 10))
+        self.mult = random.randint(1,8)
+        print "SYSTEM SELECTED: ",self.mult
+        self.length_x2 = self.mult*(self.right_x1-self.left_x1)
+        self.left_x2 = random.randint(50,380-self.length_x2)
+        self.y_x2 = random.randint(50,400)
+        self.drawBar(self.RED,self.left_x2,self.left_x2 + self.length_x2, self.y_x2)
+        self.saveLog()
+
+    def saveLog(self): 
+        f = open('log.txt','a')
+        f.write('---------------------------------------')
+        strToWrite = 'Coordinates: left_x1, right_x1, y_x1, left_x2, left_x2+length_x2, y_x2'+ \
+        str([self.left_x1, self.right_x1, self.y_x1, self.left_x2, self.left_x2+self.length_x2, self.y_x2])
+        f.write(strToWrite)
+        f.write('\n Score: '+str(self.score))
+        f.close()
+
+if __name__ == "__main__":
+    MainWindow = TestGame()
+    MainWindow.MainLoop()
